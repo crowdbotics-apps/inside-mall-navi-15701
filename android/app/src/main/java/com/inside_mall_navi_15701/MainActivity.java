2f16db83018690b92d65f4e01a27779af18610c9
Crowdbotics <team@crowdbotics.com>
2020-04-10 04:51:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/inside_mall_navi_15701/MainActivity.java
@@ -0,0 +1,15 @@
+package com.inside_mall_navi_15701;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "inside_mall_navi_15701";
+    }
+}
